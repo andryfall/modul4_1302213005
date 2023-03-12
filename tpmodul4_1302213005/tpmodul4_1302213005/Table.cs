3caf60110971762b17fdd8993ce27cd026719879
andryfall <andryfall@student.telkomuniversity.ac.id>
2023-03-12 17:55:20 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul4_1302213005/tpmodul4_1302213005/Table.cs
@@ -0,0 +1,14 @@
+﻿internal class Table
+{
+    public Table()
+    {
+    }
+    public enum Kelurahan { Batununggal, Kujangsar, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja }
+    public static int getKodePos(Kelurahan kelurahan)
+    {
+        int[] kodePos = { 40266, 40287, 40267, 40256, 40286, 40286, 40286, 40272, 40272, 40274, 40273 };
+
+
+        return kodePos[(int)kelurahan];
+    }
+}
---
+++ tpmodul4_1302213005/tpmodul4_1302213005/main.cs
@@ -0,0 +1,12 @@
+﻿using static Table;
+
+class main
+{
+    public static void Main(string[] args)
+    {
+        Table t = new Table();
+        Kelurahan kelurahan = Kelurahan.Batununggal;
+
+        Console.WriteLine(getKodePos(kelurahan));
+    }
+}
